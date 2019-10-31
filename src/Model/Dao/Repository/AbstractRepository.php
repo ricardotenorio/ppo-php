c07f9698743b962b9e3a1b90c6c589b92ffbc2e7
ricardotenorio <ricardo.heber@gmail.com>
2019-10-31 16:45:42 -0300

    AbstractRepository...

--- src/Model/Dao/Repository/AbstractRepository.php
+++ src/Model/Dao/Repository/AbstractRepository.php
@@ -10,33 +10,80 @@
     abstract class AbstractRepository
     {
         use CreateQueryTrait;
-        public function save(string $entityName, array $data): ?Exception
+
+        private $error;
+
+        abstract public function createObject(array $entity): ?AbstractEntity;
+       
+        public function save(string $entityName, array $data): void
         {
             try {
                 $connection = DatabaseConnection::getInstance();
                 if (empty($connection)){
-                    return DatabaseConnection::getError();
+                    return;
                 }
 
                 $stmt = $connection->prepare($this->insertQuery($entityName, $data));
                 $stmt->execute($data);
             } catch (PDOException $e){
-                return $e;
+                $this->error = $e;
             }
         }
 
-        public function delete(string $entityName, array $conditions)
+        public function delete(string $entityName, array $conditions): void
         {
+            try {
+                $connection = DatabaseConnection::getInstance();
+                if (empty($connection)){
+                    return;
+                }
+
+                $stmt = $connection->prepare($this->deleteQuery($entityName, $conditions));
+                $stmt->execute($conditions);
+            } catch (PDOException $e) {
+                $this->error = $e;
+            }
+        }
+
+        public function update(string $entityName, array $data, array $conditions): void
+        {
+            try {
+                $connection = DatabaseConnection::getInstance();
+                if (empty($connection)){
+                    return;
+                }
+
+                $stmt = $connection->prepare($this->updateQuery($entityName, $data, $conditions));
+                $stmt->execute($data + $conditions);
+            } catch (PDOException $e) {
+                $this->error = $e;
+            }
+        }
+
+        public function fetch(string $entityName, array $joinTables = null, array $conditions): ?array
+        {
+            $data;
+
             try {
                 $connection = DatabaseConnection::getInstance();
                 if (empty($connection)){
                     return DatabaseConnection::getError();
                 }
 
-                $stmt = $connection->prepare($this->deleteQuery($entityName, $conditions));
+                $stmt = $connection->prepare($this->selectQuery($entityName, $joinTables, $conditions));
                 $stmt->execute($conditions);
+                $data = $stmt->fetch(PDO::FETCH_ASSOC);
             } catch (PDOException $e) {
-                return $e;
+                $this->error = $e;
             }
+
+            return $data;
         }
+
+        public function getError(): ?Exception
+        {
+            return $this->error;
+        }
+
     }
+  
