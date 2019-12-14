662d39b7251649d51862048418fc8ad74c3dbd9f
ricardotenorio <ricardo.heber@gmail.com>
2019-12-14 05:52:20 -0300

    template base

--- web/_base.php
+++ web/_base.php
@@ -11,23 +11,61 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
         <?php if($v->section("menu")): 
             echo $v-section("menu");
         else:
             ?>
-            <a class="navbar-brand h1" href="#">Test</a>
+            <a class="navbar-brand h1" href="#">Project</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon">
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Postagens</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Listas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">disciplinas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Assuntos</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="justify-content-right">
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <a class="nav-link active bg-danger m-2" href="#">Sign-up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active bg-info m-2" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
         <?php
         endif; 
         ?>
     </nav>
-    
-    <main class="main">
-        <?= $v->section("content"); ?>
-    </main>
 
-    <footer class="footer">
-    </footer>
+    <div class="container">
+        <main class="main">
+            <?= $v->section("content"); ?>
+        </main>
+    </div>
+
+    <footer class="nav fixed-bottom bg-dark mt-5">
+        <p class="m-auto text-white h3 p-5">Project | 2019</p>
+    </footer>   
 
     <script src="<?= url("/web/js/jquery-3.4.1.slim.min.js") ?>"></script>
     <script src="<?= url("/web/js/bootstrap.js") ?>"></script>
