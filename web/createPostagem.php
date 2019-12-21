ad219da5cf600e353821cee94b6fd665d2c56d5f
ricardotenorio <ricardo.heber@gmail.com>
2019-12-21 19:38:33 -0300

    pagina nova postagem

--- web/createPostagem.php
+++ web/createPostagem.php
@@ -11,12 +11,12 @@
 <div class="content row justify-content-center">
 
     <h2 class="text-center col-12">Criar nova postagem</h2>
-    <div class="jumbotron text-center col-8">
+    <div class="jumbotron text-center col-sm-12 col-md-8">
         <form action="<?= url("createPostagem") ?>" method="POST">
 
             <div class="form-group col-12">
                 <label for="titulo">Título</label>
-                <input type="text" name="titulo" class="form-control" id="titulo" placeholder="Título">
+                <input type="text" name="titulo" class="form-control" id="titulo" placeholder="Título" required>
             </div>
 
             <div class="dropdown">
@@ -44,8 +44,49 @@
             </div>
 
             <div class="form-group col-12">
+                <label for="assunto">Assunto</label>
+                <input type="text" name="assunto" class="form-control" id="assunto" placeholder="assunto" required>
+            </div>
+
+            <div class="form-group col-12">
                 <label for="link">Url</label>
-                <input type="text" name="link" class="form-control" id="link" placeholder="Url">
+                <input type="text" name="link" class="form-control" id="link" placeholder="Url" required>
+            </div>
+
+            <h6 class="h6">Tipo de Mídia</h6>
+
+            <div class="form-group">
+
+                <div class="form-check form-check-inline col-12">
+                    <input class="form-input-check col-1" type="radio" id="video" name="tipo" value="video" checked>
+                    <label class="form-check-label col-sm-5 col-md-3" for="video">
+                        Video
+                    </label>
+
+                    <input class="form-input-check col-1" type="radio" id="texto" name="tipo" value="texto">
+                    <label class="form-check-label col-sm-5 col-md-3" for="texto">
+                        Texto
+                    </label>
+                </div>
+
+                <div class="form-check form-check-inline col-12">
+                    <input class="form-input-check col-1" type="radio" id="audio" name="tipo" value="audio">
+                    <label class="form-check-label col-sm-5 col-md-3" for="audio">
+                        Audio
+                    </label>
+
+                    <input class="form-input-check col-1" type="radio" id="imagem" name="tipo" value="imagem">
+                    <label class="form-check-label col-sm-5 col-md-3" for="imagem">
+                        Imagem
+                    </label>
+                </div>
+
+                <div class="form-check form-check-inline col-12">
+                    <input class="form-input-check col-1" type="radio" id="outro" name="tipo" value="outro">
+                    <label class="form-check-label col-sm-5 col-md-3" for="outro">
+                        Outro
+                    </label>
+                </div>
             </div>
 
             <div class="form-group col-12">
